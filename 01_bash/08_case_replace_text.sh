7cbd64010096b52b438fbe23cba87c850fec9ab0
Iurii-77777 <90862571+Iurii-77777@users.noreply.github.com>
2025-12-08 21:27:50 +0300

    Update 08_case_replace_text.sh

--- 01_bash/08_case_replace_text.sh
+++ 01_bash/08_case_replace_text.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+if [ $# -ne 3 ]; then
+    echo "Используйте аргументы: $0 <filename> <old_word> <new_word>"
+    exit 1
+fi
+
+filename=$1
+old_word=$2
+new_word=$3
+
+if [ ! -f "$filename" ]; then
+    echo "Файл $filename не существует"
+    exit 1
+fi
+
+# Выполняем замену
+sed -i "s/$old_word/$new_word/g" "$filename"

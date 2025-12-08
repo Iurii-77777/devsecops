1cbb814364a588a9fdfbde38e93aa2265a2e4c0f
Iurii-77777 <90862571+Iurii-77777@users.noreply.github.com>
2025-12-08 20:48:30 +0300

    Update 03_case_check_number.sh

--- 01_bash/03_case_check_number.sh
+++ 01_bash/03_case_check_number.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+echo "Введите число:"
+read number
+
+if [ $number -gt 0 ]; then
+    echo "Положительное значение"
+elif [ $number -lt 0 ]; then
+    echo "Отрицательное значение"
+else
+    echo "Ноль"
+fi

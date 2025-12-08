82a742c35f249ca92d85cf199b5b1265b8fd5c9c
Iurii-77777 <90862571+Iurii-77777@users.noreply.github.com>
2025-12-08 21:22:28 +0300

    Update 07_case_read_file.sh

--- 01_bash/07_case_read_file.sh
+++ 01_bash/07_case_read_file.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Введите путь к файлу в качестве аргумента."
+    exit 1
+fi
+
+namefile=$1
+
+if [ ! -f "$namefile" ]; then
+    echo "Файл $namefile не существует"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    echo "$line"
+done < "$namefile"

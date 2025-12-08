ebd9a96e198984cb1c8c24b6df6b242eea32c86d
Iurii-77777 <90862571+Iurii-77777@users.noreply.github.com>
2025-12-08 21:14:07 +0300

    Update 05_case_file_manager.sh

--- 01_bash/05_case_file_manager.sh
+++ 01_bash/05_case_file_manager.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+mkdir -p test
+cd test
+
+# Создаем файлы
+touch file1 file2 file3
+
+echo "Файлы созданы:"
+ls
+
+# Удаляем файлы
+rm file1 file2 file3
+
+echo "Файлы удалены"
+
+# Выходим из директории
+cd ..
